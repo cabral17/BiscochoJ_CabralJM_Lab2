7f76b056b142277d73472c345617d6f81d09b69f
Group5 <group5@gmail.com>
2021-09-21 09:09:09 +0000

    Third Part Lab 2

--- average.py
+++ average.py
@@ -13,7 +13,19 @@ Final = float(input("Finals: "))
 sum = Prelim + Midterm + Semifinal + Final
 avg = sum/4
 
-if avg>=75 and avg<=100:
-	print ("Your Average is {}" .format(avg) + " and you PASSED. You did great Job!")
+if avg>=99 and avg<=100:
+    print("Your Average is {} " .format(avg) + "you PASSED and your grade is A. Superb!")
+elif avg>=90 and avg<98:
+    print("Your Average is {} " .format(avg) + "you PASSED and your grade is B. Excellent!")
+elif avg>=80 and avg<89:
+    print("Your Average is {} " .format(avg) + "you PASSED and your grade is C. Very Good!")
+elif avg>=75 and avg<79:
+    print("Your Average is {} " .format(avg) + "you PASSED and your grade is D. Good!")
+elif avg>=70 and avg<74:
+    print("Your Average is {} " .format(avg) + "you FAILED and your grade is D. Focus and you can Pass!")
+elif avg>=61 and avg<69:
+    print("Your Average is {} " .format(avg) + "you FAILED and your grade is E. More focus on the study!")
+elif avg<60:
+    print("Your Average is {} " .format(avg) + "you FAILED and your grade is F. You need to prioritize you studies!")
 else:
-	print ("Your Average is {}" .format(avg) + " and you FAILED. Focus on your studies!")
+    print("You had entered an invalid value. Please Try Again!")
